706873c5c47ff03dc99912686d9ed45bfc1ad3f7
annabohak <50541508+annabohak@users.noreply.github.com>
2020-11-24 19:20:14 +0200

    Update script.js

--- Lab2/script.js
+++ Lab2/script.js
@@ -9,9 +9,9 @@ function hypotenuse() {
   l2 = parseFloat($('#leg2').val());
 
   if (l1 >= 0 && l2 >= 0 && l1 != '' && l2 != '') {
-    result = (l1 ** 2) + (l2 ** 2).toFixed(2);
+    result = Math.sqrt(l1 ** 2 + l2 ** 2).toFixed(2);
     $('.result-box').css('background-color', 'forestgreen');
-    $('#message').text('Результат: ');
+    $('#message').text('Гіпотенуза: ');
     $('#result').text(result);
   }
   else {

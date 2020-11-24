46be2bc8d1b87f8d9d76660442877e44c368d2e3
annabohak <50541508+annabohak@users.noreply.github.com>
2020-11-24 20:20:39 +0200

    Create script.js

---
+++ Lab2/script.js
@@ -0,0 +1,19 @@
+$(window).on('load', function () {
+  $("button").on("click", function () {
+    hypotenuse();
+  });
+});
+
+function hypotenuse() {
+  l1 = parseFloat($('#leg1').val());
+  l2 = parseFloat($('#leg2').val());
+
+  if (l1 >= 0 && l2 >= 0 && l1 != '' && l2 != '') {
+    result = Math.sqrt(l1 ** 2 + l2 ** 2).toFixed(2);
+    $('#message').text('Гіпотенуза: ');
+    $('#result').text(result);
+  }
+  else {
+    $('#message').text('Помилка! Введіть сторони ще раз');
+  }
+}
